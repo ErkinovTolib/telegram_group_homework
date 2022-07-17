f47899603aef9610f132a60595f00ec76a6e7595
ErkinovTolib <tolibjonerkinov015@gmail.com>
2022-07-17 14:36:35 +0500

    find_all_users_name

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -9,4 +9,14 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    return
+    user_name = []
+    messages = data["messages"]
+    for i in messages:
+        if i.get("actor") and i.get("actor") not in user_name:
+            user_name.append(i["actor"])
+        if i.get("from") and i.get("from") not in user_name:
+            user_name.append(i["from"])
+
+    return user_name
+
+print(find_all_users_name(read_data('data/result.json')))
